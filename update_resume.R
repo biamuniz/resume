9cc265f60d1844295ba2ba57ec15c3e03603adef
Bianca <41485608+biamuniz@users.noreply.github.com>
2023-12-02 21:37:25 -0300

    update

--- update_resume.R
+++ update_resume.R
@@ -2,8 +2,8 @@
 # date: November 19, 2019
 # intent: update the resume html and pdf file in one script
 
-rmarkdown::render(input = "resume.Rmd",
-                  output_file = "index.html",
+rmarkdown::render(input = "resume-en.Rmd",
+                  output_file = "en/index.html",
                   output_options = list(
                     css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
                     self_contained = TRUE
@@ -13,8 +13,19 @@ rmarkdown::render(input = "resume.Rmd",
                   )
             )
 
-rmarkdown::render(input = "resume.Rmd",
-                  output_file = "mleary_resume_print_to_pdf.html",
+rmarkdown::render(input = "resume-pt.Rmd",
+                  output_file = "pt/index.html",
+                  output_options = list(
+                    css = c('css/custom_resume.css', 'css/styles_html.css', 'resume'),
+                    self_contained = TRUE
+                  ),
+                  params = list(
+                    doctype = "HTML"
+                  )
+)
+
+rmarkdown::render(input = "resume-en.Rmd",
+                  output_file = "en/biancamuniz_resume_pdf_en.html",
                   output_options = list(
                     css = c('css/custom_resume.css', 'css/styles_pdf.css', 'resume')
                     ),
@@ -22,4 +33,15 @@ rmarkdown::render(input = "resume.Rmd",
                     doctype = "PDF"
                   )
 )
+
+rmarkdown::render(input = "resume-pt.Rmd",
+                  output_file = "pt/biancamuniz_resume_to_pdf_pt.html",
+                  output_options = list(
+                    css = c('css/custom_resume.css', 'css/styles_pdf.css', 'resume')
+                  ),
+                  params = list(
+                    doctype = "PDF"
+                  )
+)
+
 
